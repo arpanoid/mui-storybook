c33e64070a39f1a30eaa044e09ba7d4163f591f2
Arpan Vaidya <arpan.vaidya.tech@gmail.com>
2023-05-08 09:13:59 -0400

    CircularProgress

---
+++ stories/CircularProgress.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { CircularProgress } from '@mui/material';
+
+export default {
+  title: 'Material-UI/CircularProgress',
+  component: CircularProgress,
+};
+
+export const Default = () => <CircularProgress />;
