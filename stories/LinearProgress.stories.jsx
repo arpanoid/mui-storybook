502a8d19db9f5baf2abb7d9e53d82eb92c002c4c
Arpan Vaidya <arpan.vaidya.tech@gmail.com>
2023-05-08 09:20:44 -0400

    More Stories

---
+++ stories/LinearProgress.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { LinearProgress } from '@mui/material';
+
+export default {
+  title: 'Material-UI/LinearProgress',
+  component: LinearProgress,
+};
+
+export const Default = () => <LinearProgress />;
