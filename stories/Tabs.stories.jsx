7d99785bfa0608fb70d8c41cfec2094446e71eb1
Arpan Vaidya <arpan.vaidya.tech@gmail.com>
2023-05-08 09:45:17 -0400

    Tabs story

--- stories/Tabs.stories.jsx
+++ stories/Tabs.stories.jsx
@@ -1,37 +1,57 @@
 import React, { useState } from 'react';
-import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Tab, Tabs, Typography, Box } from '@mui/material';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default {
-  title: 'Material-UI/ToggleButton',
-  component: ToggleButton,
+  title: 'Material-UI/Tabs',
+  component: Tabs,
   parameters: {
     tags: ['autodocs'],
   },
 };
 
 export const Default = () => {
-  const [alignment, setAlignment] = useState('left');
+  const [value, setValue] = useState(0);
 
-  const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   return (
-    <ToggleButtonGroup
-      value={alignment}
-      exclusive
-      onChange={handleChange}
-      aria-label="text alignment"
-    >
-      <ToggleButton value="left" aria-label="left aligned">
-        Left
-      </ToggleButton>
-      <ToggleButton value="center" aria-label="centered">
-        Center
-      </ToggleButton>
-      <ToggleButton value="right" aria-label="right aligned">
-        Right
-      </ToggleButton>
-    </ToggleButtonGroup>
+    <>
+      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+        <Tab label="Tab 1" />
+        <Tab label="Tab 2" />
+        <Tab label="Tab 3" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Tab 1 Content
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Tab 2 Content
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Tab 3 Content
+      </TabPanel>
+    </>
   );
 };
